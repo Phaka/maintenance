53b21ae494cb473ea99eadecf4dc6524be7570d3
Werner Strydom <bloudraak@users.noreply.github.com>
2022-04-11 11:31:49 -0700

    Adds Additional Tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+# Development Tools
 yum group install "Development Tools" -y
 
+# CMake
+yum install cmake -y
+
+# Java 11
+yum install java-11-openjdk -y
+
+# .NET Core 6
+rpm -Uvh https://packages.microsoft.com/config/centos/7/packages-microsoft-prod.rpm
+yum install dotnet-sdk-6.0 -y
 
 
-gcc --version
