05b22e5978b81edc877729e331059ed88fdaae62
Werner Strydom <bloudraak@users.noreply.github.com>
2022-04-11 11:10:30 -0700

    Adds updating logic

--- patch.sh
+++ patch.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
+# Install some utils just in case its not installed
+yum install yum-utils -y
+
+# Check for updates. 
 yum check-update
 retVal=$?
-if [ $retVal -eq 100 ]; then
-    yum update
+if [ $retVal -eq 100 ]; then 
+    yum update -y
+    needs-restarting
+    retVal=$?
+    if [ $retVal -eq 1 ]; then 
+        echo "Rebooting"
+        shutdown -r +1
+    fi
 else
     echo "System is up-to-date"
 fi
