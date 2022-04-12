8cd6ee8ee0b8861d7cdbc4058ab525313069c71b
Werner Strydom <bloudraak@users.noreply.github.com>
2022-04-11 21:27:00 -0700

    Refactoring Script

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,5 +1,38 @@
 #!/bin/sh
 
+el7() {
+    yum group install "Development Tools" -y
+    yum install cmake -y
+    yum install java-11-openjdk -y
+    yum check-update
+    retVal=$?
+    if [ $retVal -eq 100 ]; then 
+        yum update -y
+        shutdown -r +1
+    else
+        echo "System is up-to-date"
+    fi
+}
+
+el9()  {
+    dnf group install "Development Tools" -y
+    dnf install cmake -y
+    dnf install java-11-openjdk -y
+    dnf check-update
+    retVal=$?
+    if [ $retVal -eq 100 ]; then 
+        dnf upgrade -y
+        shutdown -r +1
+    else
+        echo "System is up-to-date"
+    fi
+}
+
+el8()  {
+    dnf --disablerepo '*' --enablerepo=extras swap centos-linux-repos centos-stream-repos
+    el9
+}
+
 OS="`uname`"
 case $OS in
   'Linux')
@@ -11,33 +44,16 @@ case $OS in
         CentOS*)
             case $VERSION_ID in
             5|6|7)
-                yum group install "Development Tools" -y
-                yum install cmake -y
-                yum install java-11-openjdk -y
-                yum check-update
-                retVal=$?
-                if [ $retVal -eq 100 ]; then 
-                    yum update -y
-                    shutdown -r +1
-                else
-                    echo "System is up-to-date"
-                fi
+                el7
                 ;;
-            8|9)
-                dnf group install "Development Tools" -y
-                dnf install cmake -y
-                dnf install java-11-openjdk -y
-                dnf check-update
-                retVal=$?
-                if [ $retVal -eq 100 ]; then 
-                    dnf upgrade -y
-                    shutdown -r +1
-                else
-                    echo "System is up-to-date"
-                fi
+            8)
+                el8
                 ;;
             9)
-                echo "!!CentOS 9"
+                el9              
+                ;;
+            *)
+                echo "!!CentOS Linux Not Supported"
                 ;;
             esac
             ;;
