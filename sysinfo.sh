c31fa010bf62a9f7039ee05bca95d2836b06d860
Werner Strydom <bloudraak@users.noreply.github.com>
2022-04-11 20:16:50 -0700

    Getting Linux Distro

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,1 +1,38 @@
 #!/bin/sh
+
+OS="`uname`"
+case $OS in
+  'Linux')
+    OS='Linux'
+    if [ -f /etc/os-release ]; then
+        OS=$NAME
+        VER=$VERSION_ID
+    fi
+    ;;
+  'FreeBSD')
+    OS='FreeBSD'
+    ;;
+  'NetBSD')
+    OS='NetBSD'
+    ;;
+  'OpenBSD')
+    OS='OpenBSD'
+    ;;
+  'WindowsNT')
+    OS='Windows'
+    ;;
+  'Darwin') 
+    OS='Darwin'
+    ;;
+  'SunOS')
+    OS='Solaris'
+    ;;
+  'AIX') 
+    OS='AIX'
+    ;;
+  *) 
+    OS='Unkown'
+  ;;
+esac
+
+echo $OS
