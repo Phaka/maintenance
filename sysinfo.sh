6b5367677bd01daa6a4bbc37b7364c4b53b8bc53
Werner Strydom <bloudraak@users.noreply.github.com>
2022-04-11 20:43:08 -0700

    Updates

--- sysinfo.sh
+++ sysinfo.sh
@@ -10,14 +10,31 @@ case $OS in
         case $NAME in
         CentOS*)
             case $VERSION_ID in
-            6)
-                echo "!!CentOS 6"
+            5|6|7)
+                yum group install "Development Tools" -y
+                yum install cmake -y
+                yum install java-11-openjdk -y
+                yum check-update
+                retVal=$?
+                if [ $retVal -eq 100 ]; then 
+                    yum update -y
+                    shutdown -r +1
+                else
+                    echo "System is up-to-date"
+                fi
                 ;;
-            7)
-                echo "!!CentOS 7"
-                ;;
-            8)
-                echo "!!CentOS 8"
+            8|9)
+                dnf group install "Development Tools" -y
+                dnf install cmake -y
+                dnf install java-11-openjdk -y
+                dnf check-update
+                retVal=$?
+                if [ $retVal -eq 100 ]; then 
+                    dnf upgrade -y
+                    shutdown -r +1
+                else
+                    echo "System is up-to-date"
+                fi
                 ;;
             9)
                 echo "!!CentOS 9"
