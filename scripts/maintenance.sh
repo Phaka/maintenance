633c7da83225c42fca20304db6c0d268ba71f9ff
Werner Strydom <bloudraak@users.noreply.github.com>
2022-04-14 20:38:21 -0700

    Fixing Commands for OpenBSD

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -202,16 +202,16 @@ openbsd() {
   pkg_add -v gcc
   pkg_add -v git
   pkg_add -v jdk-11.0.7.10.2p0v0
-  pkg_add -y autoconf 
-  pkg_add -y automake 
-  pkg_add -y bash 
-  pkg_add -y bzip2 
-  pkg_add -y gmake 
-  pkg_add -y m4 
-  pkg_add -y meson 
-  pkg_add -y nasm 
-  pkg_add -y ninja-build 
-  pkg_add -y install python37 
+  pkg_add -v autoconf 
+  pkg_add -v automake 
+  pkg_add -v bash 
+  pkg_add -v bzip2 
+  pkg_add -v gmake 
+  pkg_add -v m4 
+  pkg_add -v meson 
+  pkg_add -v nasm 
+  pkg_add -v ninja-build 
+  pkg_add -v python37 
   pkg_add -v cmake
 }
 
