51744e729d0699f7f3491535a163e71b4a9aea31
Werner Strydom <bloudraak@users.noreply.github.com>
2022-04-16 12:46:16 -0700

    Remove -o IdentitiesOnly=yes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 HOST=$1
-ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no phaka@$HOST uname -a
-ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no phaka@$HOST rm -Rf scripts
-scp -o IdentitiesOnly=yes -rp -o StrictHostKeyChecking=no scripts phaka@$HOST:~/
-ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no phaka@$HOST chmod u+x scripts/*.sh
-ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no phaka@$HOST sudo scripts/adduser.sh $AGENT_USR $AGENT_PSW
-ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no phaka@$HOST sudo scripts/maintenance.sh
+ssh  -o StrictHostKeyChecking=no phaka@$HOST uname -a
+ssh  -o StrictHostKeyChecking=no phaka@$HOST rm -Rf scripts
+scp  -rp -o StrictHostKeyChecking=no scripts phaka@$HOST:~/
+ssh  -o StrictHostKeyChecking=no phaka@$HOST chmod u+x scripts/*.sh
+ssh  -o StrictHostKeyChecking=no phaka@$HOST sudo scripts/adduser.sh $AGENT_USR $AGENT_PSW
+ssh  -o StrictHostKeyChecking=no phaka@$HOST sudo scripts/maintenance.sh
